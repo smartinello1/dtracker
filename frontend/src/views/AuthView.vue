c1eb3f14a3297ae35deb30474a553b1299c7e363
smartinello1 <martinellosimone01@gmail.com>
2024-03-05 00:06:14 +0100

    update frontend

--- frontend/src/views/AuthView.vue
+++ frontend/src/views/AuthView.vue
@@ -1,16 +1,50 @@
 <template>
-    <div class="auth-container">
-      <h1>Sign in or Sign up</h1>
-    </div>
-  </template>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .auth-container {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
+  <div class="auth-container">
+    <h1>Sign in or Sign up</h1>
+    <label for="email"></label>
+    <input @keyup="changeEmailHandler" id="email" type="email"/>
+    {{ email }}
+    <button @click="inviteHandler">Invite</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .auth-container {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
   }
-  </style>
-  
+}
+</style>
+
+<script setup>
+
+import { supabase } from '../../lib/supabaseClient'
+import { ref } from 'vue'
+
+console.log('supabase: ' , supabase)
+
+// If supabase client not found, redirect to home
+if(!supabase) {
+    //
+}
+
+let email = ref()
+
+async function inviteHandler(event) {
+    console.log('inviteHandler: ' , event)
+    let { data, error } = await supabase.auth.signUp({
+        email: 'martinellosimone01@gmail.com',
+        password: 'example-password',
+    })
+    console.log('data invite: ', data, error)
+
+}
+
+function changeEmailHandler(event) {
+    email.value = event.target.value
+    console.log('email: ' , email.value)
+}
+
+</script>
