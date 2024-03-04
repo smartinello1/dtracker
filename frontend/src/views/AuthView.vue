6eeb2510f4de3dd133577bd236a41599777b0aad
smartinello1 <martinellosimone01@gmail.com>
2024-03-04 23:05:15 +0100

    setup vue project

---
+++ frontend/src/views/AuthView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="auth-container">
+      <h1>Sign in or Sign up</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .auth-container {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
