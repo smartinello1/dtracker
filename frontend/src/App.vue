9ff93a438f6228ba1baebd723bd4ae4c2521e8af
smartinello1 <martinellosimone01@gmail.com>
2024-07-07 10:33:46 +0200

    created navbar component, clean not used components
    Created navbar component to handle auth and unauth users
    Cleaned the repo from other unused components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,100 +1,46 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import { onMounted, ref } from 'vue'
-import router from '@/router'
-import { supabase } from '../lib/supabaseClient'
+import { RouterView } from 'vue-router'
+import Navbar from './components/ui/Navbar.vue';
+// import { onMounted, ref } from 'vue'
+// import router from '@/router'
+// import { supabase } from '../lib/supabaseClient'
 
-const session = ref()
+// const session = ref()
 
-onMounted(() => {
-  supabase.auth.getSession().then(({ data }) => {
-    session.value = data.session
-  })
+// onMounted(() => {
+//   supabase.auth.getSession().then(({ data }) => {
+//     session.value = data.session
+//   })
 
-  supabase.auth.onAuthStateChange((_, _session) => {
-    session.value = _session
-  })
-})
+//   supabase.auth.onAuthStateChange((_, _session) => {
+//     session.value = _session
+//   })
+// })
 
-async function handleClickLogout() {
-  let error = await supabase.auth.signOut()
-  console.log('error: ', error)
-  if (error.error) {
-    alert('error: ' + error.message)
-  } else {
+// async function handleClickLogout() {
+//   let error = await supabase.auth.signOut()
+//   console.log('error: ', error)
+//   if (error.error) {
+//     alert('error: ' + error.message)
+//   } else {
     
-    router.push({ path: '/' })
-  }
-}
+//     router.push({ path: '/' })
+//   }
+// }
 
-function goToLogin() {
-  router.push({ path: '/auth' })
-}
+// function goToLogin() {
+//   router.push({ path: '/auth' })
+// }
 
-function handleMenuLink(event) {
-  console.log('pathname: ', event.target.name)
-  router.push({ path: event.target.name })
-}
+// function handleMenuLink(event) {
+//   console.log('pathname: ', event.target.name)
+//   router.push({ path: event.target.name })
+// }
 
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <nav v-if="!session">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-      <RouterLink to="/auth">Auth</RouterLink>
-    </nav>
-    <nav v-else>
-      <RouterLink to="/dashboard">Dashboard</RouterLink>
-      <button @click="handleClickLogout">Logout</button>
-    </nav>
-  </header> -->
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" @click="handleMenuLink" name="/">
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-      </a>
-
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div class="navbar-menu">
-      <div class="navbar-start">
-          <a @click="handleMenuLink" name="/" class="navbar-item">
-            Home
-          </a>
-
-          <a @click="handleMenuLink" name="/about" class="navbar-item">
-            About
-          </a>
-
-          <a @click="handleMenuLink" name="/" class="navbar-item">
-            Documentation
-          </a>
-      </div>
-
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <a class="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light" @click="goToLogin">
-              Log in
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </nav>
+  <Navbar></Navbar>
   <RouterView />
 </template>
 
---
+++ frontend/src/components/ui/Navbar.vue
@@ -0,0 +1,117 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import router from '@/router'
+import { supabase } from '../../../lib/supabaseClient';
+
+const session = ref()
+
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+  })
+
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
+})
+
+async function handleClickLogout() {
+  let error = await supabase.auth.signOut()
+  console.log('error: ', error)
+  if (error.error) {
+    alert('error: ' + error.message)
+  } else {
+    
+    router.push({ path: '/' })
+  }
+}
+
+function goToLogin() {
+  router.push({ path: '/auth' })
+}
+
+function handleMenuLink(event) {
+  console.log('pathname: ', event.target.name)
+  router.push({ path: event.target.name })
+}
+
+</script>
+
+<style scoped>
+
+.navbar-item > img { 
+    max-height: 2.75rem; 
+}
+
+.navbar-item > img.is-rounded { 
+    max-height: 4rem; 
+}
+
+</style>
+
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" @click="handleMenuLink" name="/">
+                <img alt="Vue logo" @click="handleMenuLink" name="/" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+            </a>
+        </div>
+        <!-- navbar for unauthenticated users - START -->
+        <div class="navbar-menu" v-if="!session">
+            <div class="navbar-start">
+                <a @click="handleMenuLink" name="/" class="navbar-item">
+                    Home
+                </a>
+
+                <a @click="handleMenuLink" name="/about" class="navbar-item">
+                    About
+                </a>
+
+                <a @click="handleMenuLink" name="/" class="navbar-item">
+                    Documentation
+                </a>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-primary">
+                            <strong>Sign up</strong>
+                        </a>
+                        <a class="button is-light" @click="goToLogin">
+                            Log in
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- navbar for unauthenticated users - END -->
+
+        <!-- navbar for authenticated users - START -->
+        <div class="navbar-menu" v-if="session">
+            <div class="navbar-start">
+                <a @click="handleMenuLink" name="/dashboard" class="navbar-item">
+                    Dashboard
+                </a>
+
+                <a @click="handleMenuLink" name="/todos" class="navbar-item">
+                    Todos
+                </a>
+            </div>
+
+            <div class="navbar-end">
+                    <figure class="image is-64x64 navbar-item">
+                        <img class="is-rounded" src="https://bulma.io/assets/images/placeholders/128x128.png" />
+                    </figure>
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-light" @click="handleClickLogout">
+                            Log out
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- navbar for authenticated users - END -->
+    </nav>
+</template>
